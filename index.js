f2299e1fd728aca30f5dc149577a73a718e5793c
naosora <50814877+naosora@users.noreply.github.com>
2019-05-19 21:58:07 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+var recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha');
+
+function auth(){
+
+    var number = '+51' + document.querySelector('input').value;
+  
+    firebase.auth().signInWithPhoneNumber(number, recaptcha).then( function(e) {
+        console.log('Transferencia de texto exitosa');
+
+
+        var code = prompt('Por favor ingrese su número de verificación', '');
+
+        
+        if(code === null) return;
+
+        
+        e.confirm(code).then(function (result) {
+            console.log('Autenticación exitosa', result.user);
+
+            document.querySelector('label').textContent += 'Verificado----> ' + result.user.phoneNumber;
+            
+        }).catch(function (error) {
+            console.error('Autenticación fallida', error);
+            
+        });
+
+    })
+    .catch(function (error) {
+        console.error('Transferencia de texto fallida', error);
+
+    });
+  
+}
